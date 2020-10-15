b8dc15a69e808717a0db5f064214f60fca92df1b
Likit Preeyanon <likit.pre@mahidol.edu>
2020-10-15 22:55:05 +0700

    added view & page for project listing

--- src/app/projects/views.py
+++ src/app/projects/views.py
@@ -1,8 +1,9 @@
 import os
+import glob
 from flask import request, redirect, render_template, url_for, jsonify
-from flask.signals import message_flashed
 from . import project_bp as project
 
+PROJECT_DIR = os.path.join(os.getcwd(), 'projects')
 
 @project.route('/api/new')
 def create_project():
@@ -16,3 +17,9 @@ def create_project():
     else:
         os.makedirs(proj_dir)
         return jsonify(success=True)
+
+
+@project.route('/')
+def list_projects():
+    dirs = [d for d in os.listdir(PROJECT_DIR)]
+    return render_template('projects/list.html', projects=dirs)
