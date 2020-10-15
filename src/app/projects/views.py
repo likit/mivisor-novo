08f6a9bbc9c15fd3c00ed2fc0f91220cb0b09cf9
Likit Preeyanon <likit.pre@mahidol.edu>
2020-10-15 22:46:59 +0700

    added code that create a new project directory

--- src/app/projects/views.py
+++ src/app/projects/views.py
@@ -1,12 +1,18 @@
 import os
-from flask import request, redirect, render_template, url_for
+from flask import request, redirect, render_template, url_for, jsonify
+from flask.signals import message_flashed
 from . import project_bp as project
 
 
-@project.route('/new')
+@project.route('/api/new')
 def create_project():
     proj_name = request.args.get('name')
-    if os.path.exists(proj_name):
-        return 'Exists'
+    if not proj_name:
+        return jsonify(success=False, message='No project name specified.')
+    proj_dir = os.path.join('projects', proj_name)
+    proj_dir = os.path.join(os.getcwd(), proj_dir)
+    if os.path.exists(proj_dir):
+        return jsonify(success=False, message='Project already exists.')
     else:
-        return 'Not exists'
+        os.makedirs(proj_dir)
+        return jsonify(success=True)
